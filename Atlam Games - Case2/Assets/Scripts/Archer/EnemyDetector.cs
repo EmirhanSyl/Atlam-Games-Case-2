1f4cb297ff12b9290df6277e572ec9780b4acce8
EmirhanSyl <61618968+EmirhanSyl@users.noreply.github.com>
2022-06-28 18:35:40 +0300

    Archer Setup

---
+++ Atlam Games - Case2/Assets/Scripts/Archer/EnemyDetector.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDetector : MonoBehaviour
+{
+    [SerializeField] private LayerMask layerMask;
+
+    public GameObject targetEnemy;
+    private Collider[] enemyColls;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        DetectEnemy();
+        if (!targetEnemy.activeSelf)
+        {
+            targetEnemy = null;
+        }
+    }
+
+    void DetectEnemy()
+    {
+        enemyColls = Physics.OverlapSphere(transform.position, 20f, layerMask);
+
+        float distanceToClosestEnemy = Mathf.Infinity;
+        GameObject closestEnemy = null;
+
+        foreach (var currentEnemyColl in enemyColls)
+        {
+            float distanceToCurrentEnemy = (currentEnemyColl.gameObject.transform.position - transform.position).sqrMagnitude;
+            if (distanceToCurrentEnemy < distanceToClosestEnemy)
+            {
+                distanceToClosestEnemy = distanceToCurrentEnemy;
+                closestEnemy = currentEnemyColl.gameObject;
+                targetEnemy = closestEnemy;
+            }
+        }
+    }
+}
